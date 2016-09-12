bc3f75e11962095f0a6e540c89a0348f70f59272
Cleverson <cleverson.s.dias@gmail.com>
2016-09-12 20:47:17 -0300

    initial commit

---
+++ app/Models/Categoria.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Categoria extends Model
+{
+    protected $table = ['categorias'];
+
+    protected $fillable = ['nome'];
+
+    /**
+     * Get the produtos for the Produto.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function produtos()
+    {
+        return $this->hasMany(Produto::class);
+    }
+}
---
+++ app/Models/Produto.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Produto extends Model
+{
+    protected $table = ['produtos'];
+
+    protected $fillable = ['nome','descricao','preco_custo','preco_venda'];
+
+    /**
+     * Get the categoria that owns the Categoria.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function categoria()
+    {
+        return $this->belongsTo(Categoria::class);
+    }
+}
